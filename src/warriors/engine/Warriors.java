293d9ad822a8b624e1f4d1d4eb59755daee21c65
christopheleduc <christophe.leduc@le-campus-numerique.fr>
2019-05-29 16:58:53 +0200

    My 4ème commit

--- src/warriors/engine/Warriors.java
+++ src/warriors/engine/Warriors.java
@@ -1,6 +1,7 @@
 package warriors.engine;
 
 import java.util.List;
+import java.util.ArrayList;
 
 import warriors.contracts.GameState;
 import warriors.contracts.GameStatus;
@@ -8,16 +9,46 @@ import warriors.contracts.Hero;
 import warriors.contracts.Map;
 import warriors.contracts.WarriorsAPI;
 
-public class Warriors implements GameState, WarriorsAPI {
+public class Warriors implements GameState, WarriorsAPI, Hero, Map {
+
+    private ArrayList list = new ArrayList<Object>();
+    private String playerName;
+    private String image;
+    private String hero;
+    private String gameId;
+    
+    public Warriors() {
+        this("Conan", "conan.png", "Guerrier", 0, 0);
+    }
+
+    public Warriors(String playerName, String image, String hero) {
+        this(playerName, image, hero, 0, 0);
+    }
+
+    public Warriors(String playerName, String image, String hero, int life, int force) {
+        this.setPlayerName(playerName) ;
+        this.setImage(image) ;
+        this.setHero(hero) ;
+        this.setLife(r.nextInt(max - min + 1) + min) ;
+        this.setForce(r.nextInt(max - min + 1) + min) ;
+    }
 
     @Override
     public String getPlayerName() {
-        return null;
+        return playerName;
+    }
+
+    public void setPlayerName(String playerName) {
+        this.playerName = playerName ;
     }
 
     @Override
     public String getGameId() {
         return null;
+    }
+
+    public void setGameId(String gameId) {
+        this.gameId = gameId ;
     }
 
     @Override
@@ -27,7 +58,7 @@ public class Warriors implements GameState, WarriorsAPI {
 
     @Override
     public Hero getHero() {
-        return null;
+        return hero;
     }
 
     @Override
@@ -65,5 +96,33 @@ public class Warriors implements GameState, WarriorsAPI {
         return null;
     }
 
+    @Override
+    public int getNumberOfCase() {
+        return 0;
+    }
+
+    @Override
+    public String getName() {
+        return null;
+    }
+
+    @Override
+    public String getImage() {
+        return null;
+    }
+
+    public void setImage(String image) {
+        this.image = image ;
+    }
+
+    @Override
+    public int getLife() {
+        return 0;
+    }
+
+    @Override
+    public int getAttackLevel() {
+        return 0;
+    }
     
 }
